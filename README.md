19023fed428d736ff7fff1f5850fd9e0dda59da4
ucipass@gmail.com <ucipass@gmail.com>
2022-01-30 15:46:44 -0600

    SAN addition

--- README.md
+++ README.md
@@ -3,18 +3,22 @@ This program has two use cases:
 - Update namecheap DNS records using a YAML file
 - Use it with letsencrypt certbot -manual-auth-hook and --manual-clean-up script to authenticate certificates.
 
-# Mandatory environment variables
-NAMECHEAP_USER=[USER]
-NAMECHEAP_APIKEY=[APIKEY]
-NAMECHEAP_SUBDOMAIN=[sub-domain e.g. example]
-NAMECHEAP_TOPDOMAIN=[top level domain e.g com]
+# Environment variables
+- NAMECHEAP_USER=[USER]
+- NAMECHEAP_APIKEY=[APIKEY]
+- NAMECHEAP_SUBDOMAIN=[sub-domain e.g. example]
+- NAMECHEAP_TOPDOMAIN=[top level domain e.g com]
+- CERT_FQDN=[Fully qualified domain name of the certificate]
+- CERT_SAN=[Subject Alternative Name, optinal]
+- CERT_EMAIL=[Email for certificate registration]
+- CERT_DIR=[certbot certificate directory]
 
 # Certbot update example (use --test-cert for testing)
-certbot -d test.example.com \
---text --non-interactive --agree-tos --email postamster@example.com \
---config-dir ./certificates \
---work-dir   ./certificates \
---logs-dir   ./certificates  \
+certbot -d \$CERT_FQDN,\$CERT_SAN \
+--text --non-interactive --agree-tos --email \$CERT_EMAIL \
+--config-dir \$CERT_DIR \
+--work-dir   \$CERT_DIR \
+--logs-dir   \$CERT_DIR  \
 --manual-public-ip-logging-ok \
 --manual-auth-hook    "./namecheap.py --add" \
 --manual-cleanup-hook "./namecheap.py --delete"  \
--- cert_renew.sh
+++ cert_renew.sh
@@ -3,22 +3,40 @@
 # Change into script directory
 cd "${0%/*}"
 
-
 # Load environment variables from .env
 # Use --test-cert for testing
-
+# export $(xargs <.env)
 # export $(grep -v '^#' .env | xargs -d '\n')
+
+echo $CERT_SAN
+
+if [ -n $CERT_SAN ]; then
+
+    certbot -d $CERT_FQDN,$CERT_SAN  \
+    --text --non-interactive --agree-tos --email $CERT_EMAIL \
+    --config-dir $CERT_DIR \
+    --work-dir   $CERT_DIR \
+    --logs-dir   $CERT_DIR \
+    --manual-public-ip-logging-ok \
+    --manual-auth-hook    "./namecheap.py --add" \
+    --manual-cleanup-hook "./namecheap.py --delete"  \
+    --manual --preferred-challenges dns \
+    certonly
+
+else
+    certbot -d $CERT_FQDN \
+    --text --non-interactive --agree-tos --email $CERT_EMAIL \
+    --config-dir $CERT_DIR \
+    --work-dir   $CERT_DIR \
+    --logs-dir   $CERT_DIR \
+    --manual-public-ip-logging-ok \
+    --manual-auth-hook    "./namecheap.py --add" \
+    --manual-cleanup-hook "./namecheap.py --delete"  \
+    --manual --preferred-challenges dns \
+    certonly
+
+fi
+
 
 # ./namecheap.py --add
 
-certbot -d $CERT_FQDN \
---text --non-interactive --agree-tos --email $CERT_EMAIL \
---config-dir $CERT_DIR \
---work-dir   $CERT_DIR \
---logs-dir   $CERT_DIR \
---manual-public-ip-logging-ok \
---manual-auth-hook    "./namecheap.py --add" \
---manual-cleanup-hook "./namecheap.py --delete"  \
---manual --preferred-challenges dns \
---test-cert \
-certonly
